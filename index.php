eb080b45d5ac0945f3750345f4ddce4389cfe4c3
LonglifeHyun <44224844+LonglifeHyun@users.noreply.github.com>
2020-01-08 16:50:59 +0900

    Second Version

--- index.php
+++ index.php
@@ -5,10 +5,22 @@
   </head>
   <body>
     <h1><a href="index.php">WEB</a></h1>
-    <ol>
+    <!-- <ol>
       <li><a href="index.php?id=HTML">HTML</a></li>
       <li><a href="index.php?id=CSS">CSS</a></li>
       <li><a href="index.php?id=JavaScript">JacaScript</a></li>
+    </ol> -->
+    <ol>
+      <?php
+      $lists = scandir('data');
+      foreach ($lists as $list) {
+        if(in_array($list, ['.','..']))
+          continue;
+          ?>
+          <li><a href="index.php?id=<?=$list?>"><?=$list?></a></li>
+          <?php
+      }
+       ?>  
     </ol>
 
     <h2>
@@ -24,7 +36,16 @@
     <h2>
     <?php
     if(isset($_GET['id'])) {
+
       echo file_get_contents("data/".$_GET['id']);
+      //echo "1";
+      if($_GET['id']=='HTML'){
+        echo "Good";
+      } elseif ($_GET['id']=='CSS') {
+        echo "Wow";
+      } else {
+        echo "I don't know";
+      }
     } else {
       echo "Hello, PHP!";
     }
